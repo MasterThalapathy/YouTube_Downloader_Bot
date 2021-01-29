9694602e2f1f5d57133e50c2ba8d0e22cd1880ef
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-01-29 17:59:01 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"\n1.Send Me Your Youtube Link And Select Desired Option To Be Uploaded To Telegram \n2. Currently Only Supports Youtube Single  (No playlist) Just Send Any Youtube Link \n\n<b>📜Quote : </b><code>കിടന്ന് അടി വയ്ക്കരുത് എല്ലാർക്കും ഉപയോഗിക്കാൻ പറ്റും😌</code>"
+    helptxt = f"\n➠ Send Me Your Youtube Link And Select Desired Option To Be Uploaded To Telegram \n➠ Currently Only Supports Youtube Single  (No playlist) Just Send Any Youtube Link \n\n<b>📜Quote : </b><code>കിടന്ന് അടി വയ്ക്കരുത് എല്ലാർക്കും ഉപയോഗിക്കാൻ പറ്റും😌</code>"
     await message.reply_text(helptxt)
