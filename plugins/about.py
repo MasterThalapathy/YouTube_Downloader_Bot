4104e3eb630a1d48437e684d6c49e3d651fbadf3
MasterThalapathy <75004401+MasterThalapathy@users.noreply.github.com>
2021-02-27 20:49:04 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,3 +1,14 @@
-Import os
+from script import script
 
-@Client.on_message(filters.command(["about"]))def about(bot, update): bot.send_message( chat_id=update.chat.id, text=script.ABOUT_TEXT, parse_mode="html", reply_to_message_id=update.message_id, disable_web_page_preview=True ) 
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command(["about"]))
+def about(bot, update):
+
+    bot.send_message(
+        chat_id=update.chat.id, 
+        text=script.ABOUT_TEXT,
+        parse_mode="html", 
+        reply_to_message_id=update.message_id, 
+        disable_web_page_preview=True 
+    ) 
