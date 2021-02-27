5177e86b62ec3761edee1344e2d29b758b538b49
MasterThalapathy <75004401+MasterThalapathy@users.noreply.github.com>
2021-02-27 21:21:53 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,12 +1,24 @@
-from script import script
+By #masterThalapathy
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters
+from script import script  
 
-@Client.on_message(filters.command(["about"]))
-def about(bot, update):
+@Client.on_message(filters.command(["about"]) & filters.private)
+ async def about(client, message):
 
-    bot.send_message(
-        chat_id=update.chat.id, 
-        text=script.ABOUT_TEXT,
-        parse_mode="html", 
-        reply_to_message_id=update.message_id, 
-        disable_web_page_preview=True 
-    ) 
+    try:
+        await message.reply_text(
+            text=script.ABOUT_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="help_data"),
+                    ],
+                
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
